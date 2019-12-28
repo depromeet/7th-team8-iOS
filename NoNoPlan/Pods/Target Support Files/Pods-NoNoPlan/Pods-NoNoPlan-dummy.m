e6912638c4568f3fedd98cc3906d682efdc0fb01
jeong jin ho <yyyqor@gmail.com>
2019-12-28 15:34:14 +0900

    Pod init

---
+++ NoNoPlan/Pods/Target Support Files/Pods-NoNoPlan/Pods-NoNoPlan-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NoNoPlan : NSObject
+@end
+@implementation PodsDummy_Pods_NoNoPlan
+@end
---
+++ NoNoPlan/Pods/Target Support Files/Pods-NoNoPlanTests/Pods-NoNoPlanTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NoNoPlanTests : NSObject
+@end
+@implementation PodsDummy_Pods_NoNoPlanTests
+@end
