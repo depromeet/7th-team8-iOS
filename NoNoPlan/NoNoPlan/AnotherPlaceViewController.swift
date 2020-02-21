d4afa0a4e268c16839b4489539f4d06c0a406e57
jeong jin ho <yyyqor@gmail.com>
2020-02-21 20:06:13 +0900

    메인 UI수정

---
+++ NoNoPlan/NoNoPlan/AnotherPlaceViewController.swift
@@ -0,0 +1,9 @@
+//
+//  anotherPlaceViewController.swift
+//  NoNoPlan
+//
+//  Created by 진호놀이터 on 2020/02/15.
+//  Copyright © 2020 진호놀이터. All rights reserved.
+//
+
+import Foundation
---
+++ NoNoPlan/NoNoPlan/ReviewViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ReviewViewController.swift
+//  NoNoPlan
+//
+//  Created by 진호놀이터 on 2020/02/08.
+//  Copyright © 2020 진호놀이터. All rights reserved.
+//
+
+import Foundation
